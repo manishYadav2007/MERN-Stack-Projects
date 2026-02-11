e6a9e566c8b843f37eca3a65d667daefcdbade32
= <=>
2026-02-11 19:06:26 +0530

    Ad env port

--- chatify-hub/backend/server.js
+++ chatify-hub/backend/server.js
@@ -5,11 +5,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
+const port = process.env.PORT || 3000;
+console.log(port);
 
+const app = express();
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port 3000`);
 });
