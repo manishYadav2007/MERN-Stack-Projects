5852ca29d8443ac97ffa3386b99720c9b874471a
= <=>
2026-02-11 12:12:29 +0530

    clean up the repo, remove some dir

--- Full-Stack Chat App/backend/src/routes/message.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const route = express.Router();
-
-route.get("/send", (request, response) => {
-  response.send("Send Message API Endpoint");
-});
-
-export default route;
