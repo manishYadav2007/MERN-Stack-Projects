2c51893d40046e4bc58a48467ef92967edfc67e9
= <=>
2026-02-12 15:31:41 +0530

    setup production code

--- chatify-hub/backend/server.js
+++ chatify-hub/backend/server.js
@@ -1,17 +1,29 @@
 import express from "express";
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
+import path from "path";
 import dotenv from "dotenv";
 
+const app = express();
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-console.log(port);
+const __dirname = path.resolve();
 
-const app = express();
+const port = process.env.PORT || 3000;
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+// Make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port 3000`);
 });
+
