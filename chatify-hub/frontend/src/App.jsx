8c7c6a347dd25d60993c1b199d2f8e3a633badc7
= <=>
2026-02-19 13:58:02 +0530

    Set up the frontend codebase

--- chatify-hub/frontend/src/App.jsx
+++ chatify-hub/frontend/src/App.jsx
@@ -1,10 +1,33 @@
 // import { useState } from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore";
 
 function App() {
+  const { authUser, isLoggedIn, login } = useAuthStore();
+
+  console.log(`Auth User: ${authUser}`);
+  console.log(`isLoggedIn: ${isLoggedIn}`);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+        <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+        <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+        <button className="z-10 btn btn-info" onClick={login}>
+          {isLoggedIn ? "Logout" : "Login"}
+        </button>
+
+        <Routes>
+          <Route path="/" element={<ChatPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ chatify-hub/frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <button className='btn btn-danger'>Login Page</button>
+    </div>
+  )
+}
+
+export default LoginPage
