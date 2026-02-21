00a1a2c9ffff5260b4b4c8da134687928e1a7479
= <=>
2026-02-21 13:22:00 +0530

    Setting up the chat page requires specific states.

---
+++ chatify-hub/frontend/src/components/NoConversionPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoConversionPlaceholder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NoConversionPlaceholder
---
+++ chatify-hub/frontend/src/store/useChatStore.js
@@ -0,0 +1,57 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set, get) => ({
+  allContacts: [],
+  chats: [],
+  messages: [],
+  activeTab: "chats",
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+  isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
+
+  // actions
+  toggleSound: () => {
+    const isSoundEnabled = !get().isSoundEnabled;
+    localStorage.setItem("isSoundEnabled", JSON.stringify(isSoundEnabled));
+    set({ isSoundEnabled });
+  },
+  
+
+  setActiveTab: (tab) => set({ activeTab: tab }),
+  setSelectedUser: (user) => set({ selectedUser: user }),
+
+  getAllContacts: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const response = await axiosInstance.get("messages/contacts");
+      set({ allContacts: response.data });
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.messages ||
+        "Network Error: Unable to fetch contacts";
+      toast.error(errorMessage);
+      console.error(`Error fetching contacts: ${errorMessage}`);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getChatPartners: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const response = await axiosInstance.get("messages/chats");
+      set({ chats: response.data });
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.messages ||
+        "Network Error: Unable to fetch chat partners";
+      toast.error(errorMessage);
+      console.error(`Error fetching chat partners: ${errorMessage}`);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+}));
